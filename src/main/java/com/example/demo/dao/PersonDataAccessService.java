7228af546a5a825e7e103d063b17d49b61dc790c
Junhyung Ki <kijoonh91@gmail.com>
2020-12-25 23:32:06 +0100

    spring client api for postgreSQL done (1:42:10)

--- src/main/java/com/example/demo/dao/PersonDataAccessService.java
+++ src/main/java/com/example/demo/dao/PersonDataAccessService.java
@@ -1,6 +1,8 @@
 package com.example.demo.dao;
 
 import com.example.demo.model.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
@@ -10,6 +12,14 @@ import java.util.UUID;
 
 @Repository("postgres")
 public class PersonDataAccessService implements PersonDao {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public PersonDataAccessService(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
     @Override
     public int insertPerson(UUID id, Person person) {
         return 0;
@@ -17,10 +27,12 @@ public class PersonDataAccessService implements PersonDao {
 
     @Override
     public List<Person> selectAllPeople() {
-        final Person person = new Person(UUID.randomUUID(), "FROM POSTGRES DB");
-        List<Person> list = new ArrayList<Person>();
-        list.add(person);
-        return list;
+        final String sql = "SELECT id, name FROM person";
+        return jdbcTemplate.query(sql, (resultSet, i) -> {
+            final UUID id = UUID.fromString(resultSet.getString("id"));
+            final String name = resultSet.getString("name");
+            return new Person(id, name);
+        });
     }
 
     @Override
